46f02879652e50f3449cb088cda797a1cd7955a6
ColinBerryMD <colin.berry.md@gmail.com>
2024-01-26 22:52:37 -0700

    almost there

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for 
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
-from models import User
+from os import environ
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv, find_dotenv
    
 app = Flask(__name__)  
+load_dotenv(find_dotenv())
 
 
-#app.config['SECRET_KEY'] = environ['WTF_SECRET']
-app.config['SECRET_KEY'] = 'not_very_secret_key'
+app.config['SECRET_KEY'] = environ['WTF_SECRET']
+#app.config['SECRET_KEY'] = 'not_very_secret_key'
 
 # uncomment this to use SQLite for development
 #basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,20 +21,42 @@ app.config['SECRET_KEY'] = 'not_very_secret_key'
 #db_url = 'mysql+pymysql://webserver:'+db_password+'@localhost/sms'
 
 # uncomment this to use mysql for with unprivileged account
-db_password = environ.get('MYSQL_WEBSERVER_PASSWORD')
-db_url = 'mysql+pymysql://webserver:'+db_password+'@localhost/purple_noodle'
+#db_password = environ.get('MYSQL_WEBSERVER_PASSWORD')
+#db_url = 'mysql+pymysql://webserver:'+db_password+'@localhost/purple_noodle'
 
-# uncomment this to use mysql and modify 'fluffy-waffle' database
-#db_password = environ.get('MYSQL_PRIVILEGED_PASSWORD')
-#db_url = 'mysql+pymysql://privileged:'+db_password+'@localhost/purple_noodle'
+# uncomment this to use mysql and modify 'purple_noodle' database
+db_password = environ.get('MYSQL_PRIVILEGED_PASSWORD')
+db_url = 'mysql+pymysql://privileged:'+db_password+'@localhost/purple_noodle'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
+bcrypt = Bcrypt()
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
-
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)    
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    location = db.Column(db.String(500))
+    time = db.Column(db.String(100))
+
+@login_manager.user_loader
+def load_user(user_id):
+# since the user_id is just the primary key of our user table, use it in the query for the user
+	return User.query.get(int(user_id))
 
 # blueprint for auth routes in our app
 from auth import auth as auth_blueprint
--- auth.py
+++ auth.py
@@ -1,16 +1,98 @@
-from flask import Blueprint, render_template
-
-from app import db
+from app import db, bcrypt, User, Blueprint, render_template, request, flash, redirect, url_for, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods= ( 'GET','POST'))
 def login():
+    if request.method == 'POST':
+
+    # login code goes here 
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        try:
+            user = User.query.filter_by(email=email).one()
+        except:
+            flash('User not found.')
+            return render_template('login.html')  # if the user doesn't exist or password is wrong, reload the page 
+
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it to the hashed password in the database
+        if not bcrypt.check_password_hash( user.password, password ):
+            flash('Please check your login details and try again.')
+            return render_template('login.html')  # if the user doesn't exist or password is wrong, reload the page
+
+        # if the above check passes, then we know the user has the right credentials
+        login_user(user, remember=remember)
+        return redirect(url_for('main.profile'))
+
+    # GET request
     return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods= ('GET','POST'))
 def signup():
+
+    if request.method == 'POST':
+    # code to validate and add user to database goes here
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+
+        user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+        
+        if user: # if a user is found, we want to redirect back to signup page so user can try again
+            flash('That email is already registered.')
+            return redirect(url_for('auth.signup'))
+        
+        if not ( password == confirm ):
+            flash('Passwords must match.')
+            return redirect(url_for('auth.signup')) 
+
+        # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+        hash = bcrypt.generate_password_hash(password)
+        new_user = User(email=email, name=name, password=hash)
+        
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+                
+        return redirect(url_for('auth.login'))
+
     return render_template('signup.html')
+
+@auth.route('/new_password', methods= ('GET','POST'))
+@login_required
+def new_password():
+
+    if request.method == 'POST':
+    # code to validate and add user to database goes here
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+
+        user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+        
+        if user: # if a user is found, we want to redirect back to signup page so user can try again
+            flash('That email is already registered.')
+            return redirect(url_for('auth.signup'))
+        
+        if not ( password == confirm ):
+            flash('Passwords must match.')
+            return redirect(url_for('auth.signup')) 
+
+        # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+        hash = bcrypt.generate_password_hash(password)
+        new_user = User(email=email, name=name, password=hash)
+        
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+                
+        return redirect(url_for('auth.login'))
+
 
 @auth.route('/logout')
 def logout():
